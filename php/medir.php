0bebc1f0171675be53f5945cbaa88da735e9666a
riverajefer <jefersonpatino@yahoo.es>
2015-09-15 15:45:10 -0500

    Comentarios

--- php/medir.php
+++ php/medir.php
@@ -1,3 +1,7 @@
 <?php
+// Ejecuta como root, el script python encargado de controlar el sensor ultrasonido
+
 echo exec('sudo python /var/www/ultrasonido/python/ultrasonido.py');
+
+// La salida la imprime con el echo
 ?>
--- python/ultrasonido.py
+++ python/ultrasonido.py
@@ -1,38 +1,45 @@
-import RPi.GPIO as GPIO                    #Import GPIO library
-import time                                #Import time library
-GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering 
+""""
+Tomado en gran parte de:
+https://electrosome.com/hc-sr04-ultrasonic-sensor-raspberry-pi/
 
-TRIG = 23                                  #Associate pin 23 to TRIG
-ECHO = 24                                  #Associate pin 24 to ECHO
+"""
+
+
+import RPi.GPIO as GPIO                    
+import time                                
+GPIO.setmode(GPIO.BCM)                     
+
+TRIG = 23                                  #pin 23 como TRIG
+ECHO = 24                                  #pin 24 como ECHO
 
 print "Medicion de la distancia en curso"
 
-GPIO.setup(TRIG,GPIO.OUT)                  #Set pin as GPIO out
-GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
+GPIO.setup(TRIG,GPIO.OUT)                  #TRIG como salida
+GPIO.setup(ECHO,GPIO.IN)                   #ECHO como entrada
 
-GPIO.output(TRIG, False)                   #Set TRIG as LOW
+GPIO.output(TRIG, False)                   #TRIG en estado bajo
 print "Espere que el sensor se estabilice"
-time.sleep(2)                              #Delay of 2 seconds
+time.sleep(2)                              #Esperar 2 segundos
 
-GPIO.output(TRIG, True)                    #Set TRIG as HIGH
-time.sleep(0.00001)                        #Delay of 0.00001 seconds
-GPIO.output(TRIG, False)                   #Set TRIG as LOW
+GPIO.output(TRIG, True)                    #TRIG en estado alto
+time.sleep(0.00001)                        #Delay de 0.00001 segundos
+GPIO.output(TRIG, False)                   #TRIG en estado bajo
 
-while GPIO.input(ECHO)==0:                 #Check whether the ECHO is LOW
-  pulse_start = time.time()                #Saves the last known time of LOW pulse
+while GPIO.input(ECHO)==0:                 #Comprueba si ECHO está en estado bajo
+  pulse_start = time.time()                #Guarda el tiempo transcurrido, mientras esta en estado bajo
 
-while GPIO.input(ECHO)==1:                 #Check whether the ECHO is HIGH
-  pulse_end = time.time()                  #Saves the last known time of HIGH pulse 
+while GPIO.input(ECHO)==1:                 #Comprueba si ECHO está en estado alto
+  pulse_end = time.time()                  #Guarda el tiempo transcurrido, mientras esta en estado alto
 
-pulse_duration = pulse_end - pulse_start   #Get pulse duration to a variable
+pulse_duration = pulse_end - pulse_start   #Se obtienen la duración del pulso, calculando la diferencia entre pulse_start  y pulse_end
 
-distance = pulse_duration * 17150          #Multiply pulse duration by 17150 to get distance
-distance = round(distance, 2)              #Round to two decimal points
+distance = pulse_duration * 17150          #Se multiplica la duración del pulso, por 17150, para obetener la distancia
+distance = round(distance, 2)              #Se redondea a dos decimales
 
-if distance > 2 and distance < 400:        #Check whether the distance is within range
+if distance > 2 and distance < 400:        #Comprueba si la distancia está dentro del rango
   distancia = distance - 0.5;
-  print "Distancia: ",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
+  print "Distancia: ",distance - 0.5,"cm"  #Imprime la distancia con 0.5 de calibración
 else:
-  print "Fuera de Rango"                   #display out of range
+  print "Fuera de Rango"                   #Imprime fuera de rango
 
-GPIO.cleanup()
+GPIO.cleanup()							   #Limpia los pines	
