b8cc4edebe6461e743313c5485cfd96738cb4c66
rpi-jefer <riverajefer@gmail.com>
2015-09-16 23:56:12 -0500

    Ajuste PHP

--- php/medir.php
+++ php/medir.php
@@ -1,7 +1,7 @@
 <?php
 // Ejecuta como root, el script python encargado de controlar el sensor ultrasonido
 
-echo exec('sudo python /var/www/ultrasonido/python/ultrasonido.py');
+echo exec('sudo python /var/www/rpi-ultrasonido/python/ultrasonido.py');
 
 // La salida la imprime con el echo
 ?>
